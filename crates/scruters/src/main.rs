e9810fd7aad5020667714ae5aa68789dc5e34298
future-highway <113635015+future-highway@users.noreply.github.com>
2024-07-31 15:48:36 -0400

    feat: setup color_eyre for panics & errors

--- crates/scruters/src/main.rs
+++ crates/scruters/src/main.rs
@@ -1,12 +1,17 @@
 //! Scruters is a TUI with various tools for Rust
 //! development.
 
+use color_eyre::{config::HookBuilder, eyre, Result};
 use state::State;
+use std::panic;
 
 mod state;
+mod tui;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    install_hooks()?;
+
     let state = if let Some(state) =
         State::load_from_file().await
     {
@@ -17,5 +22,39 @@ async fn main() {
         state
     };
 
-    println!("{state:?}");
+    let _tui = tui::init()?;
+
+    tui::restore()?;
+
+    todo!("{state:?}");
+}
+
+/// This replaces the standard `color_eyre` panic and error
+/// hooks with hooks that restore the terminal before
+/// printing the panic or error.
+///
+/// Source: <https://ratatui.rs/recipes/apps/color-eyre/>
+fn install_hooks() -> Result<()> {
+    // add any extra configuration you need to the hook
+    // builder
+    let hook_builder = HookBuilder::default();
+    let (panic_hook, eyre_hook) = hook_builder.into_hooks();
+
+    // convert from a color_eyre PanicHook to a standard
+    // panic hook
+    let panic_hook = panic_hook.into_panic_hook();
+    panic::set_hook(Box::new(move |panic_info| {
+        drop(tui::restore()); // ignore any errors as we are already failing
+        panic_hook(panic_info);
+    }));
+
+    // convert from a color_eyre EyreHook to a eyre
+    // ErrorHook
+    let eyre_hook = eyre_hook.into_eyre_hook();
+    eyre::set_hook(Box::new(move |error| {
+        drop(tui::restore()); // ignore any errors as we are already failing
+        eyre_hook(error)
+    }))?;
+
+    Ok(())
 }
